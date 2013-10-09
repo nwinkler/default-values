b2865c6a4b1ffbcf24f4a4846a93a013014a25f7
Nils Winkler <nils@nilswinkler.com>
2013-10-09 17:28:16 +0200

    Added first basic test.

--- test/test-defaults.js
+++ test/test-defaults.js
@@ -1,6 +1,10 @@
+var defaultValues = require('..');
+var assert = require('assert');
 
 describe('Default Value', function () {
   it('should create a new object', function() {
+    var cfg = defaultValues();
 
+    assert.ok(cfg);
   });
 });
